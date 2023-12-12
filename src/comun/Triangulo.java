a4610c0dddec87cb77097813e476dffcec1e14aa
alex <alex@1DAW-05>
2023-12-12 13:28:43 +0100

    0

--- src/comun/Triangulo.java
+++ src/comun/Triangulo.java
@@ -11,9 +11,23 @@ public class Triangulo {
 			System.out.println("Intro lado 2");
 			int ladoc2=in.nextInt();
 			System.out.println(calcularArea(ladoc1, ladoc2));
+			System.out.println("Introduce el diametro de la circunferencia");
+			double diametro = in.nextDouble();
+			System.out.println("introduce el radio de la circunferencia");
+			double radio = in.nextDouble();
+			calcularPerimetroyRadio(diametro,radio);
 		}
 		public static double calcularArea(int lado1, int lado2) {
 			return (lado1*lado2)/2;
 			
 		}
+		public static void calcularPerimetroyRadio(double radio, double diametro) {
+			double pi = 3.14;
+			double perimetro = 2*pi*radio;
+			double area = pi * radio * radio;
+			System.out.println("el perimetro de la circunferencia es: "+ perimetro+" el area de la circunferencia es: "+ area);
+		}
+		
+
+
 }
